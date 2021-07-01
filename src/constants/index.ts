b3bdd6a84ab54bf2a006a58f4af387e72e7e0f7c
Hoang Nong <hoang.nong.it@gmail.com>
2021-07-01 09:51:22 +0000

    Update code

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { fortmatic, injected, binanceinjected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x10ed43c718714eb63d5aa57b78b54704e256024e'
+export const ROUTER_ADDRESS = '0xd96e72971649715bdd35259d711dbe6a5dc5f086'
 
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
